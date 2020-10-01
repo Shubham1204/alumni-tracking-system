723bdb5cdbc2a3143691343d7efcc4a7e06745f9
12shubhamgupta1999 <40001567+12shubhamgupta1999@users.noreply.github.com>
2020-10-01 12:55:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>Linear by TEMPLATED</title>
+		<title>Alumni Tracking System</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<!-- <meta name="description" content="" />
 		<meta name="keywords" content="" /> -->
